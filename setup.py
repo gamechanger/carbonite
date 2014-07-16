acf689a3a304ea6f0b028d87b31d579649598f66
Travis Thieman <travis.thieman@gmail.com>
2014-07-16 11:46:52 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='carbonite',
+      version='0.0.1',
+      description='Freeze your Python package dependencies',
+      author='GameChanger',
+      author_email='kiril@gc.io',
+      license='MIT',
+      packages=['carbonite'],
+      tests_require=['nose'],
+      test_suite='nose.collector',
+      entry_points={'console_scripts': ['carbonite = carbonite.freeze:main']},
+      zip_safe=False)
---
+++ tests/dynamic_requirements.py
@@ -0,0 +1,5 @@
+__carbonite__ = ['INSTALL_REQUIRES', 'TEST_REQUIRES']
+
+INSTALL_REQUIRES = ['Dockets >= 0.0.40a', 'requests>=1.2.0']
+
+TEST_REQUIRES = ['nose >= 0.0.1']
