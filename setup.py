dbbfbdd248321b07f0b22d2b017647b143b6f071
Travis Thieman <travis.thieman@gmail.com>
2014-07-18 11:45:32 -0400

    Process dependency links

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='carbonite',
-      version='0.0.3',
+      version='0.0.4',
       description='Freeze your Python package dependencies',
       url='https://github.com/gamechanger/carbonite',
       author='GameChanger',
