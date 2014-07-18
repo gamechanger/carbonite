71e9868131b1d59a3467ba9c264af9292e99b07a
Travis Thieman <travis.thieman@gmail.com>
2014-07-18 14:18:11 -0400

    Still trying really hard

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='carbonite',
-      version='0.0.6',
+      version='0.0.7',
       description='Freeze your Python package dependencies',
       url='https://github.com/gamechanger/carbonite',
       author='GameChanger',
