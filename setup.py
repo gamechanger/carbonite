456ac771de84c4b01386f1964eb2729ccac78089
Travis Thieman <travis.thieman@gmail.com>
2014-07-18 11:34:58 -0400

    Send --upgrade to  pip install command

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='carbonite',
-      version='0.0.2',
+      version='0.0.3',
       description='Freeze your Python package dependencies',
       url='https://github.com/gamechanger/carbonite',
       author='GameChanger',
